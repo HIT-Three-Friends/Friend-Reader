9a679ba348ae12f2bee8d8382c445628d0a73bc4
Akaisorani <1224067801@qq.com>
2017-11-13 12:43:52 +0800

    update spider add topics

--- social/sp_base.py
+++ social/sp_base.py
@@ -12,7 +12,13 @@ class basespider(object):
 	def loadConfig(self):
 		self.socialRoot="./social/"
 		self.allConfig=configparser.ConfigParser(interpolation=None)
-		self.allConfig.read(self.socialRoot+"configspider.properties",encoding='utf-8')
+		self.allConfig.read(self.socialRoot+"config.properties",encoding='utf-8')
+		try:
+			self.spiderConfig=configparser.ConfigParser(interpolation=None)
+			self.spiderConfig.read(self.socialRoot+"spider.properties",encoding='utf-8')
+		except Exception as e:
+			logging.info("spider.properties not exist, ignored")
+			
 		
 	def prepare(self):
 		logging.basicConfig(format='%(asctime)s --%(lineno)s -- %(levelname)s:%(message)s', level=logging.DEBUG)
