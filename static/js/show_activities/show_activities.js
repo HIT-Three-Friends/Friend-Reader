d82369a882640566b940e47d925f57ec2ac73070
Yukun Ma <gmayukun@gmail.com>
2017-11-14 22:13:55 +0800

    nearly finished show_activities

--- static/js/show_activities/show_activities.js
+++ static/js/show_activities/show_activities.js
@@ -1,12 +1,43 @@
 var last_activity = null;
 var current_page = 0;
+function add_activity(activity){
+    var new_activity = $("#event-template").clone();
+    new_activity.css("display", "block");
+    $("#timeline").append(new_activity);
+}
+function add_time(time) {
+    var new_time = $("#time-template").clone();
+    new_time.css("display", "block");
+    new_time.find("#time").html(time);
+    $("#timeline").append(new_time);
+}
 function loadData(){
     var totalheight = parseFloat($(window).height()) + parseFloat($(window).scrollTop());
-    if ($(document).height() <= totalheight) {  // 说明滚动条已达底部
+    console.log("load");
+    console.log($(document).height());
+    console.log(totalheight);
+    if ($(document).height() - 101 <= totalheight) {  // 说明滚动条已达底部
         current_page++;
         console.log("load more");
+        $(".footer").css("display", "block");
         $.ajax({
-
+            url: "/activities/",
+            type: "GET",
+            data: {
+                "page": current_page
+            },
+            success: function(data) {
+                var activities = data["activity"];
+                for (var i in activities) {
+                    var activity = activities[i];
+                    if (last_activity == null || activity["date"] != last_activity["date"]) {
+                        add_time(activity["date"]);
+                    }
+                    last_activity = activity;
+                    add_activity(activity);
+                }
+                $(".footer").css("display", "none");
+            }
         });
     }
 }
